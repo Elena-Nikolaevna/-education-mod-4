0ca80590901869b632bec74bbc61d08aff81cbe4
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-06-26 00:21:51 +0300

    Create callback.js

---
+++ js/callback.js
@@ -0,0 +1,26 @@
+//функции обратного вызова
+/* const doMath = function (a, b, callback) {
+    const result = callback(a, b);
+    console.log(result);
+};
+const add = function (x, y) {
+    return x + y;
+};
+const sub = function (x, y) {
+    return x - y;
+};
+doMath(2, 3, add);
+doMath(10, 8, sub);
+ */
+// Можно сделать анонимную Инлайн функцию
+/* const doMath = function (a, b, callback) {
+    const result = callback(a, b);
+    console.log(result);
+};
+doMath(2, 3, function (x, y) {
+    return x + y;
+});
+doMath(10, 8, function (x, y) {
+    return x - y;
+});
+ */
