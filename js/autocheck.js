8a59d1a9d4e2b622a4017fd5ab7077901fe9efd1
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-06-26 23:02:56 +0300

    Update autocheck.js

--- js/autocheck.js
+++ js/autocheck.js
@@ -104,4 +104,58 @@ calculateTotalPrice([164, 48, 291]) //возвращает 503
 calculateTotalPrice([412, 371, 94, 63, 176]) //возвращает 1116
 const numbers = [5, 10, 15, 20, 25]; */
 
+// 11/48
+/* const filterArray = (numbers, value) => {
+    const filteredNumbers = [];
 
+    numbers.forEach((number) => {
+        if (number > value) {
+            filteredNumbers.push(number);
+        }
+    });
+
+    // Change code above this line
+    //return filteredNumbers;
+    console.log (filteredNumbers)
+}
+filterArray([1, 2, 3, 4, 5], 3); //возвращает [4, 5]
+filterArray([1, 2, 3, 4, 5], 4); //возвращает [5]
+filterArray([1, 2, 3, 4, 5], 5); //возвращает []
+filterArray([12, 24, 8, 41, 76], 38); //возвращает [41, 76]
+filterArray([12, 24, 8, 41, 76], 20); //возвращает [24, 41, 76] */
+
+// 12/48
+/* const getCommonElements = (firstArray, secondArray) => {
+    const commonElements = [];
+
+    firstArray.forEach((element) => {
+        if (secondArray.includes(element)) {
+            commonElements.push(element);
+        }
+    });
+
+    // Change code above this line
+    //return commonElements;
+    console.log (commonElements)
+}
+getCommonElements([1, 2, 3], [2, 4]); //возвращает [2]
+getCommonElements([1, 2, 3], [2, 1, 17, 19]); // возвращает [1, 2]
+getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]); // возвращает [12, 27, 3]
+getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]); //возвращает [10, 30, 40]
+getCommonElements([1, 2, 3], [10, 20, 30]); //возвращает [] */
+
+// 13/48
+function changeEven(numbers, value) {
+  // Change code below this line
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 2 === 0) {
+      numbers[i] = numbers[i] + value;
+    }
+  }
+  console.log(numbers)
+  // Change code above this line
+}
+ changeEven([1, 2, 3, 4, 5], 10) //возвращает новый массив [1, 12, 3, 14, 5]
+ changeEven([2, 8, 3, 7, 4, 6], 10) //возвращает новый массив [12, 18, 3, 7, 14, 16]
+ changeEven([17, 24, 68, 31, 42], 100) //возвращает новый массив [17, 124, 168, 31, 142]
+ changeEven([44, 13, 81, 92, 36, 54], 100)// возвращает новый массив [144, 13, 81, 192, 136, 154]
